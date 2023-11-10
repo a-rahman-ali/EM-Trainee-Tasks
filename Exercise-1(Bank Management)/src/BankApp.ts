73410676fc89f433c432c832483b1d094621a305
smd-abdul-rahman <smdarahman55@gmail.com>
2023-11-11 02:21:55 +0530

    bankApp ready for first review

---
+++ Exercise-1(Bank Management)/src/BankApp.ts
@@ -0,0 +1,51 @@
+import promptSync from "prompt-sync";
+const prompt = promptSync();
+
+import { createAccount, displayAllAccountsDetails, viewCustomerDetails, withdrawMoney, depositMoney } from "./bankOps";
+import{ viewBalance } from "./bankOps";
+function start() {
+  console.log("Welcome to My Banking Application!!");
+
+  while(true){
+    console.log("Choose from the below options: ");
+    console.log("1. Open Savings or Current Account");
+    console.log("2. View Balance");
+    console.log("3. View Customer Details");
+    console.log("4. Withdraw Money");
+    console.log("5. Deposit Money");
+    console.log("6. Display Details of All Customers");
+    console.log("7. Exit from Application");
+    
+    let option:number = (Number)(prompt("Enter your Option: "))
+    switch (option) {
+      case 1:
+        createAccount();
+        break;
+      case 2:
+        viewBalance();
+        break;
+      case 3:
+        viewCustomerDetails();
+        break;
+      case 4:
+        withdrawMoney();
+        break;
+      case 5:
+        depositMoney();
+        break;
+      case 6:
+        displayAllAccountsDetails();
+        break;
+      case 7:
+        console.log("Thanks for using my Application");
+        process.exit(0);
+      default:
+        console.log("Please choose a valid option");
+        console.log();
+        // break;
+    }
+  }
+}
+
+// Application starts from here !!!
+start();
